a9c6577d3022ee1b723a54c467a625bda26b8301
SaadTK <tahmid.karim.saad@gmail.com>
2025-07-30 18:53:18 +0600

    Added routes for educators

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import CourseDetails from "./pages/student/CourseDetails";
 import MyEnrollments from "./pages/student/MyEnrollments";
 import VideoPlayer from "./pages/student/VideoPlayer";
 import Loading from "./components/student/Loading";
+
+import Educator from "./pages/educator/Educator";
+import Dashboard from "./pages/educator/Dashboard";
+import AddCourse from "./pages/educator/AddCourse";
+import SrudentEnrolled from "./pages/educator/SrudentEnrolled";
 const App = () => {
   return (
     <div>
       <Routes>
-
         {/* students routes  */}
         <Route path="/" element={<Home />}></Route>
         <Route path="/course-list" element={<CoursesList />}></Route>
@@ -19,14 +23,14 @@ const App = () => {
         <Route path="/my-enrollments" element={<MyEnrollments />}></Route>
         <Route path="/video-player/:courseId" element={<VideoPlayer />}></Route>
         <Route path="/loading/:path" element={<Loading />}></Route>
-      
-      {/* Educator nested routes  */}
-        <Route path="/educator" element={<Educator/>}>
 
-
-
-      </Route>
-      
+        {/* Educator nested routes  */}
+        <Route path="/educator" element={<Educator />}>
+          <Route path="/educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="students-enrolled" element={<SrudentEnrolled />} />
+        </Route>
       </Routes>
     </div>
   );
