fa419467778fbd4e8f996d995f4f62905e5ff8a5
SaadTK <tahmid.karim.saad@gmail.com>
2025-08-06 19:42:55 +0600

    fixed the courseCard section. the thumbnail and other numbers were not showing cause I gave courseTestimonial instead of dummyCoureses.

--- src/components/student/CourseCard.jsx
+++ src/components/student/CourseCard.jsx
@@ -1,28 +1,44 @@
 import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
-import { AppContext } from "../../context/AddContext";
+import { AddContext } from "../../context/AddContext";
+import { Link } from "react-router-dom";
 
 const CourseCard = ({ course }) => {
-  const { currency } = useContext(AppContext);
+  const { currency } = useContext(AddContext);
 
   return (
-    <div>
-      <img src={course.courseThumbnail} alt={course.courseTitle} />
+    <Link
+      className="border border-gray-500/30 pb-6 overflow-hidden rounded-lg"
+      to={"/course/" + course._id}
+      onClick={() => scrollTo(0, 0)}
+    >
+      <img
+        className="w-full"
+        src={course.courseThumbnail}
+        alt={course.courseTitle}
+      />
 
-      <div>
-        <h3>{course.courseTitle}</h3>
-        <p>{course.educator.name}</p>
+      <div className="p-3 text-left">
+        <h3 className="text-base text-black font-semibold">
+          {course.courseTitle}
+        </h3>
+        {/* <p className="text-gray-500">{course.educator_name}</p> */}
 
-        <div>
-          <p>4.5</p>
-          <div>
+        <div className="flex items-center space-x-2">
+          <p className="text-black">4.5</p>
+          <div className="flex">
             {[...Array(5)].map((_, i) => (
-              <img key={i} src={assets.star} alt="ratings" />
+              <img
+                className="w-3.5 h-3.5"
+                key={i}
+                src={assets.star}
+                alt="ratings"
+              />
             ))}
           </div>
-          <p>22</p>
+          <p className="text-gray-500">22</p>
         </div>
-        <p>
+        <p className="text-base font-semibold text-gray-800">
           {currency}
           {(
             course.coursePrice -
@@ -30,7 +46,7 @@ const CourseCard = ({ course }) => {
           ).toFixed(2)}
         </p>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/student/CoursesSection.jsx
+++ src/components/student/CoursesSection.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AddContext } from "../../context/AddContext";
+import CourseCard from "./CourseCard";
 
 const CoursesSection = () => {
+  const { allCourses } = useContext(AddContext);
+
   return (
     <div className="py-16 md:px-40 px-8">
       <h2 className="txt-3xl font-medium text-gray-500 mb-5">
@@ -12,6 +16,12 @@ const CoursesSection = () => {
         and design to business and wellness, our courses are crafted to deliver
         results.
       </p>
+
+      <div className="grid grid-cols-4 px-4 md:px-0 md:my-16 my-10 gap-4">
+        {allCourses.slice(0, 4).map((course, i) => (
+          <CourseCard key={i} course={course} />
+        ))}
+      </div>
 
       <Link
         className=" mb-5 text-gray-500 border border-gray-500/30 px-10 py-3 rounded"
