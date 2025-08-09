bf814adc0c0c242af85436ddcbd08f051a3f3553
SaadTK <tahmid.karim.saad@gmail.com>
2025-08-09 18:49:19 +0600

    Footer finished

--- src/pages/student/CoursesList.jsx
+++ src/pages/student/CoursesList.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const CoursesList = () => {
   return (
-    <div className="text-black">
-      <h1>Courses List Page</h1>
-    </div>
+    <>
+     
+    
+    
+    </>
   );
 };
 
