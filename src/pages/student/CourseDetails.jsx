302f171eb36e6648d1ac74056cda21951249de8c
SaadTK <tahmid.karim.saad@gmail.com>
2025-09-09 18:00:02 +0600

    almost done the courseDetails page

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -10,6 +10,7 @@ const CourseDetails = () => {
 
   const [courseData, setCourseData] = useState(null);
   const [openSections, setOpenSections] = useState({});
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false);
 
   const {
     allCourses,
@@ -38,7 +39,7 @@ const CourseDetails = () => {
 
   return courseData ? (
     <>
-      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-center md:px-36 px-8 md:pt-30 pt-20 text-left">
         <div className="absolute top-0 left-0 w-full h-10 -z-[-1] bg-gradient-to-b from-cyan-100/70"></div>
 
         {/* left column */}
@@ -181,7 +182,7 @@ const CourseDetails = () => {
 
         {/* right column */}
 
-        <div className="max-w-course-card max-w-[424px] w-full z-10 shadow-custom-card shadow-xs rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]">
+        <div className="ml-20 max-w-course-card max-w-[424px] w-full z-10 shadow-custom-card shadow-xl rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px] ">
           <img src={courseData.courseThumbnail} alt="Course Thumbnail" />
 
           <div className="p-5">
@@ -195,8 +196,6 @@ const CourseDetails = () => {
                 <span className="font-medium">5 days</span> left at this price!
               </p>
             </div>
-
-            {/* one */}
 
             <div className="flex gap-3 items-center pt-2">
               <p className="text-gray-800 md:text-4xl text-2xl font-semibold">
@@ -214,39 +213,44 @@ const CourseDetails = () => {
               </p>
             </div>
 
-            {/* one end  */}
-            {/* two  */}
             <div className="flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500">
               <div className="flex items-center gap-1">
                 <img src={assets.star} alt="star icon" />
                 <p>{calculateRating(courseData)}</p>
               </div>
 
-              {/* divider  */}
               <div className="h-4 w-px bg-gray-500/40"></div>
-              {/* divider end */}
-              {/* </div> */}
 
-              {/* three  */}
               <div className="flex items-center gap-1">
                 <img src={assets.time_clock_icon} alt="time icon" />
                 <p>{calculateCourseDuration(courseData)}</p>
               </div>
-              {/* three end */}
 
-              {/* divider  */}
               <div className="h-4 w-px bg-gray-500/40"></div>
-              {/* divider end */}
 
-              {/* four  */}
               <div className="flex items-center gap-1">
                 <img src={assets.lesson_icon} alt="lesson icon" />
                 <p>{calculateNoOfLectures(courseData)} lessons</p>
               </div>
+            </div>
 
-              {/* four end */}
+            {/* Enroll button  */}
 
-              {/* two end  */}
+            <button className="md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium">
+              {isAlreadyEnrolled ? "Already Enrolled" : "Enroll Now"}
+            </button>
+
+            <div className="pt-6">
+              <p className="md:text-xl text-lg font-medium text-gray-800">
+                What's in the course?
+              </p>
+              <ul className="ml-4 pt-2 text-sm md:text-[18px] list-disc text-gray-500">
+                <li>Lifetime access with free updates.</li>
+                <li>Step-by-step, hands-on project guidance.</li>
+                <li>Downloadable resources and source code.</li>
+                <li>Quizzes to test your knowledge.</li>
+                <li>Certificate of completion.</li>
+              </ul>
             </div>
           </div>
         </div>
